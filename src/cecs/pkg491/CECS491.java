765a02a38388c94928220a5ebbc01ab947bee6bb
Blake Hice <blake.hice@live.com>
2016-09-07 22:18:21 -0700

    added a comboBox bitch

--- src/cecs/pkg491/CECS491.java
+++ src/cecs/pkg491/CECS491.java
@@ -5,10 +5,16 @@
  */
 package cecs.pkg491;
 
+
 import javafx.application.Application;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 /**
@@ -16,22 +22,38 @@ import javafx.stage.Stage;
  * @author Blake
  */
 public class CECS491 extends Application {
+
+    public CECS491() {
+        
+    }
    
    @Override
    public void start(Stage stage) throws Exception {
+      final ObservableList <String> sorter = FXCollections.observableArrayList();
+      sorter.add("First Name");
+      sorter.add("Last Name");
+      sorter.add("Practice");
+      sorter.add("Email");
+      sorter.add("Address");
       Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
       
       Scene scene = new Scene(root);
       
+      ComboBox choice = (ComboBox)scene.lookup("#comboBox1");
+      choice.setItems(sorter);
       stage.setScene(scene);
+      stage.setTitle("Attorney Lookup");
       stage.show();
+      
    }
 
-   /**
-    * @param args the command line arguments
-    */
+   
    public static void main(String[] args) {
+     
+     
       launch(args);
+      
    }
    
+  
 }
