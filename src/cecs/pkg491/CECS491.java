19a892eb88aac306fa570206ba26a75c1e3730b9
Blake Hice <blake.hice@live.com>
2016-09-09 01:18:16 -0700

    fixed controller class

--- src/cecs/pkg491/CECS491.java
+++ src/cecs/pkg491/CECS491.java
@@ -5,16 +5,10 @@
  */
 package cecs.pkg491;
 
-
 import javafx.application.Application;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 /**
@@ -23,37 +17,24 @@ import javafx.stage.Stage;
  */
 public class CECS491 extends Application {
 
-    public CECS491() {
+    @Override
+    public void start(Stage stage) throws Exception {
+      FXMLDocumentController customControl = new FXMLDocumentController();
+        customControl.init();
         
-    }
-   
-   @Override
-   public void start(Stage stage) throws Exception {
-      final ObservableList <String> sorter = FXCollections.observableArrayList();
-      sorter.add("First Name");
-      sorter.add("Last Name");
-      sorter.add("Practice");
-      sorter.add("Email");
-      sorter.add("Address");
-      Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-      
-      Scene scene = new Scene(root);
-      
-      ComboBox choice = (ComboBox)scene.lookup("#comboBox1");
-      choice.setItems(sorter);
-      stage.setScene(scene);
-      stage.setTitle("Attorney Lookup");
-      stage.show();
-      
-   }
+        stage.setScene(new Scene(customControl));
+        stage.setTitle("Attorney Lookup");
+        
+        stage.show();
 
-   
-   public static void main(String[] args) {
-     
-     
-      launch(args);
-      
-   }
-   
-  
+        
+
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+
+    }
+
 }
--- src/cecs/pkg491/FXMLDocumentController.java
+++ src/cecs/pkg491/FXMLDocumentController.java
@@ -5,20 +5,63 @@
  */
 package cecs.pkg491;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 /**
  *
  * @author Blake
  */
-public class FXMLDocumentController implements Initializable {
+public class FXMLDocumentController extends AnchorPane{
 
-   
-   @Override
-   public void initialize(URL url, ResourceBundle rb) {
-      // TODO
-   }   
-   
+    private final String fileName = "spreadSheet.xls";
+    private ObservableList<String> firstNames = FXCollections.observableArrayList();
+    private ObservableList<String> lastNames = FXCollections.observableArrayList();
+    private ObservableList<String> practices = FXCollections.observableArrayList();
+    private ObservableList<String> sorter = FXCollections.observableArrayList();
+
+    @FXML private ComboBox sortBy;
+    @FXML private ComboBox values;
+
+    public FXMLDocumentController() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+ 
+    public void init() {
+        initializeSorter(sorter);
+        sortBy.setItems(sorter);
+    }
+
+    public void initializeSorter(ObservableList<String> sorter) {
+        sorter.add("First Name");
+        sorter.add("Last Name");
+        sorter.add("Practice");
+        sorter.add("Email");
+        sorter.add("Address");
+    }
+
+    public void initializeLists(String name, ObservableList<String> list) {
+
+    }
+
 }
