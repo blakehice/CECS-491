b240e9b039c412aa2979b7a143bf45af6f919e45
adamwills2 <adam.wills2@gmail.com>
2016-11-14 14:05:29 -0800

    :shipit:

--- src/cecs/pkg491/Person.java
+++ src/cecs/pkg491/Person.java
@@ -16,8 +16,8 @@ import javafx.beans.property.SimpleDoubleProperty;
 public class Person {
 
     private final SimpleStringProperty fname, lname;
-    private SimpleStringProperty practice, phone, email, streetAddress, company,
-        suite, city,zipcode,state;
+    private SimpleStringProperty practice, phone, email, address, company,
+        suite, city ,state, zipcode;
     private SimpleDoubleProperty miles;
     private SimpleIntegerProperty years, Attorneys;
 
@@ -28,14 +28,23 @@ public class Person {
         this.phone = new SimpleStringProperty(phone);
     }
 
-    public Person(String fname, String lname, String practice, String phone, String email, String address) {
+    public Person(String fname, String lname, String company, String address, 
+            String suite, String city, String state, String zipcode,String phone, 
+            String practice, String email, double miles, int years, int Attorneys) {
         this.fname = new SimpleStringProperty(fname);
         this.lname = new SimpleStringProperty(lname);
-        this.practice = new SimpleStringProperty(practice);
+        this.company = new SimpleStringProperty(company);
+        this.address = new SimpleStringProperty(address);
+        this.suite = new SimpleStringProperty(suite);
+        this.city = new SimpleStringProperty(city);
+        this.state = new SimpleStringProperty(state);
+        this.zipcode = new SimpleStringProperty(zipcode);
         this.phone = new SimpleStringProperty(phone);
+        this.practice = new SimpleStringProperty(practice);
         this.email = new SimpleStringProperty(email);
-        this.streetAddress = new SimpleStringProperty(address);
-
+        this.miles = new SimpleDoubleProperty(miles);
+        this.years = new SimpleIntegerProperty(years);
+        this.Attorneys = new SimpleIntegerProperty(Attorneys);
     }
 
     public String getFirstName() {
@@ -45,6 +54,34 @@ public class Person {
     public String getLastName() {
         return lname.get();
     }
+    
+    public String getCompany() {
+       return company.get();
+    }
+    
+    public String getAddress() {
+       return address.get();
+    }
+    
+    public String getSuite() {
+       return suite.get();
+    }
+    
+    public String getCity() {
+       return city.get();
+    }
+    
+    public String getState() {
+       return state.get();
+    }
+    
+    public String getZIP() {
+       return zipcode.get();
+    }
+    
+    public String getPhone() {
+       return phone.get();
+    }
 
     public String getPractice() {
         return practice.get();
@@ -53,29 +90,71 @@ public class Person {
     public String getEmail() {
         return email.get();
     }
-
-    public String getPhone() {
-        return phone.get();
+    
+    public double getMiles() {
+       return miles.get();
+    }
+    
+    public int getYears() {
+       return years.get();
     }
 
-    public String getAddress() {
-        return streetAddress.get();
+    public int getAttorneys() {
+       return Attorneys.get();
+    }
+    
+    public void setCompany(String company) {
+       this.company = new SimpleStringProperty(company);
+    }
+    
+    public void setAddress(String address) {
+       this.address = new SimpleStringProperty(address);
+    }
+    
+    public void setSuite(String suite) {
+       this.suite = new SimpleStringProperty(suite);
+    }
+    
+    public void setCity(String city) {
+       this.city = new SimpleStringProperty(city);
     }
 
-    public void setPractice(String practice) {
-        this.practice = new SimpleStringProperty(practice);
+    public void setState(String state) {
+        this.state = new SimpleStringProperty(state);
+    }
+    
+    public void setZIP(String zipcode) {
+       this.zipcode = new SimpleStringProperty(zipcode);
     }
 
     public void setPhone(String phone) {
         this.phone = new SimpleStringProperty(phone);
     }
-
-    public void setAddress(String address) {
-        this.streetAddress = new SimpleStringProperty(address);
+    
+    public void setPractice(String practice) {
+       this.practice = new SimpleStringProperty(practice);
+    }
+    
+    public void setEmail(String email) {
+       this.email = new SimpleStringProperty(email);
+    }
+    
+    public void setMiles(double miles) {
+       this.miles = new SimpleDoubleProperty(miles);
+    }
+    
+    public void setYears(int years) {
+       this.years = new SimpleIntegerProperty(years);
+    }
+    
+    public void setAttorneys(int Attorneys) {
+       this.Attorneys = new SimpleIntegerProperty(Attorneys);
     }
 
     public String toString() {
-        return fname + " " + lname + " " + practice + " " + phone + " "
-                + email + " " + streetAddress;
+        return fname + " " + lname + " " + company + " " + address + " " +
+                suite + " " + city + " " + state + " " + zipcode + " " + phone +
+                " " + practice + " " + email + " " + miles + " " + years + " " +
+                Attorneys;
     }
 }
